711b7817c657dd0e5f3ed71ea5e37bd667f98474
VJ_Hegde <99704202+vishakh-hegde-106@users.noreply.github.com>
2023-07-28 22:19:08 +0530

    repeating numbers in an arraty

---
+++ arrays/duplicates.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int i;
+        unordered_set <int> s;
+        for(int i=0;i<nums.size();i++)
+        {
+            if (s.find(nums[i]) != s.end())
+            {
+                return true;
+            }
+            s.insert(nums[i]);
+        }
+        return false;
+       
+        
+    }
+};
